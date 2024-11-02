2bed236dc88b466a5cb58f159ff3ee4939443e2d
Md Wahiduzzaman Bappy <wahidbappy253@gmail.com>
2024-11-03 02:08:37 +0600

    Code added

--- lib/main.dart
+++ lib/main.dart
@@ -1,212 +1,146 @@
 import 'package:flutter/material.dart';
 
-// Drawer, NavigationBar, Bottom Navigation bar, SingleChildScrollView, ScrollBar, ListView, ListViewBuilder
-
 void main() {
-  runApp(MyApp());
+  runApp(DemoApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'Hello World App',
-      home: HomeScreen(),
-    );
-  }
-}
+class DemoApp extends StatelessWidget {
+  DemoApp({super.key});
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   List<String> friendList = [
     'Iram',
-    'Shabbin',
+    'Sabbir',
     'Rakib',
     'Hasan',
     'Roy',
     'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
+    'Kopil',
+    'Umama',
+    'Bappy',
+    'Antor',
+    'Saiful',
+    'Saiful',
+    'Saiful',
+    'Saiful',
+    'Saiful',
   ];
+
+  TextEditingController _emailTEController = TextEditingController();
+  TextEditingController _passswordTEController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
-          // IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
-        ],
-        // leading: Icon(Icons.add),
-      ),
-      drawer: const Drawer(
-        shadowColor: Colors.red,
-        backgroundColor: Colors.white54,
-        width: 300,
-        child: Column(
-          children: [Text('dfsdf')],
-        ),
-      ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   currentIndex: 0,
-      //   selectedItemColor: Colors.pink,
-      //   unselectedItemColor: Colors.blue,
-      //   unselectedFontSize: 12,
-      //   showUnselectedLabels: false,
-      //   showSelectedLabels: true,
-      //   onTap: (int selectedIndex) {
-      //     // TODO: have to change state, will see after stateful widget
-      //   },
-      //   items: [
-      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-      //     BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-      //   ],
-      // ),
-      bottomNavigationBar: NavigationBar(
-          selectedIndex: 1,
-          onDestinationSelected: (int selectedIndex) {
-            // TODO: have to change state, will see after stateful widget
-          },
-          destinations: [
-            const NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            const NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
-          ]),
-      /*body: Scrollbar(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-            ],
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Demo App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Home',
+            style: TextStyle(color: Colors.white),
           ),
+          centerTitle: true,
+          backgroundColor: Colors.amber,
+          elevation: 10,
         ),
-      ),*/
-      body: Scrollbar(
-        thickness: 10,
-        radius: const Radius.circular(10),
-        interactive: true,
-        /* child: ListView(
-          scrollDirection: Axis.vertical,
-          reverse: false,
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          children: [
-            Text('first'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('last'),
-          ],
-        ),*/
-        /* child: ListView.builder(
-          itemCount: 100, // 100-1 = 99 => 0 -> 99
-          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        /*  body: ListView.builder(
+          itemCount: friendList.length,
           itemBuilder: (context, index) {
-            return Text('item $index');
+            int displayIndex = index + 1;
+            return ListTile(
+              dense: false,
+              title: Text(friendList[index]),
+              subtitle: Text('Friend no $displayIndex'),
+              trailing: const Icon(Icons.arrow_circle_right_outlined),
+              leading: const CircleAvatar(
+                backgroundColor: Colors.amber,
+                child: Icon(Icons.person),
+              ),
+              onTap: () {
+                print('On Tap $index');
+              },
+              onLongPress: () {
+                print('On LongPress $index');
+              },
+              tileColor: Colors.grey,
+            );
           },
-        ),*/
-        child: ListView.builder(
-          itemCount: friendList.length, // 5 (0-4)
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
+        ),*/ /*
+        body: Center(
+          child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, mainAxisSpacing: 12, crossAxisSpacing: 8),
+            children: [
+              Text('Iram'),
+              Text('Sabbir'),
+              Text('Rakib'),
+              Text('Hasan'),
+              Text('Hasan'),
+              Text('Hasan'),
+              Text('Hasan'),
+              Text('Hasan'),
+              Text('Hasan'),
+            ],*/
+        /*body: GridView.builder(
+          gridDelegate:
+              const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+          itemCount: friendList.length,
+          itemBuilder: (context, index) {
+            return Center(
               child: Text(friendList[index]),
             );
           },
+        ),*/
+        body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  controller: _emailTEController,
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                  ),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Enter Email Address';
+                    }
+                    return null;
+                  }),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator:(String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Enter Password';
+                  }
+                  return null;
+                } ,
+                controller: _passswordTEController,
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  /*String email = _emailTEController.text;
+                  String password = _passswordTEController.text;
+
+                  if (email.isNotEmpty && password.isNotEmpty) {
+                    print('Login Success');
+                  } else {
+                    print('Login Failed. Missing Data');
+                  }*/
+                  if (_formKey.currentState!.validate()) {
+                    print('Login Success');
+                  }
+                },
+                child: const Text('Tap'),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-// Suggestion => ctl + space
