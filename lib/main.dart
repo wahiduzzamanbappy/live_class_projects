5e1f1d921baac96e5a1cb59265943a4a85a1114a
Md Wahiduzzaman Bappy <wahidbappy253@gmail.com>
2024-11-03 02:07:08 +0600

    Code added

--- lib/main.dart
+++ lib/main.dart
@@ -1,212 +1,147 @@
+
 import 'package:flutter/material.dart';
 
-// Drawer, NavigationBar, Bottom Navigation bar, SingleChildScrollView, ScrollBar, ListView, ListViewBuilder
-
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'Hello World App',
-      home: HomeScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Home(),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<String> friendList = [
-    'Iram',
-    'Shabbin',
-    'Rakib',
-    'Hasan',
-    'Roy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-    'Niloy',
-  ];
+class Home extends StatelessWidget {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(Icons.edit)),
-          // IconButton(onPressed: () {}, icon: Icon(Icons.delete)),
-        ],
-        // leading: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
       ),
-      drawer: const Drawer(
-        shadowColor: Colors.red,
-        backgroundColor: Colors.white54,
-        width: 300,
+      body: Center(
         child: Column(
-          children: [Text('dfsdf')],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 16,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                  textStyle:
+                  TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                  padding: EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  shadowColor: Colors.blue,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  )),
+              onPressed: () {
+                showDialog(
+                  barrierColor: Colors.green,
+                  barrierDismissible: false,
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      //title: Text('Delete'),
+                      content: const Text('Anonna is My Wife!'),
+                      actions: [
+                        TextButton(onPressed: () {}, child: const Text('No')),
+                        TextButton(onPressed: () {}, child: const Text('Yes')),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: const Text('Tap'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextButton(
+              onPressed: () {
+                print('Text Button');
+              },
+              child: Text('Click here'),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            IconButton(
+              onPressed: () {
+                print('Icon Button');
+              },
+              icon: Icon(Icons.add),
+            ),
+            const SizedBox(height: 16),
+            OutlinedButton(
+              onPressed: () {
+                print('Outlined Button');
+              },
+              child: Text('Outline'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              onTap: () {
+                print('Inkwell');
+              },
+              child: Text('Sample Text'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: (){
+                print('Just One Click');
+              },
+
+              onDoubleTap: (){
+                print('Double Click');
+              },
+              onLongPress: (){
+                print('on Long press');
+              },
+              onLongPressEnd: (details) {
+                print('on Long press End');
+              },onLongPressCancel: (){
+              print('on Long press Cancel');
+            },
+              child: const Column(
+                children: [
+                  Text('Just One Click'),
+                  Text('Double Click'),
+                  Text('on Long press'),
+                  Text('on Long press End'),
+                  Text('on Long press Cancel'),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+          ],
         ),
       ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   currentIndex: 0,
-      //   selectedItemColor: Colors.pink,
-      //   unselectedItemColor: Colors.blue,
-      //   unselectedFontSize: 12,
-      //   showUnselectedLabels: false,
-      //   showSelectedLabels: true,
-      //   onTap: (int selectedIndex) {
-      //     // TODO: have to change state, will see after stateful widget
-      //   },
-      //   items: [
-      //     BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-      //     BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
-      //   ],
-      // ),
-      bottomNavigationBar: NavigationBar(
-          selectedIndex: 1,
-          onDestinationSelected: (int selectedIndex) {
-            // TODO: have to change state, will see after stateful widget
-          },
-          destinations: [
-            const NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
-            const NavigationDestination(icon: Icon(Icons.search), label: 'Search'),
-          ]),
-      /*body: Scrollbar(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-              Text('sdfdsfd'),
-            ],
-          ),
-        ),
-      ),*/
-      body: Scrollbar(
-        thickness: 10,
-        radius: const Radius.circular(10),
-        interactive: true,
-        /* child: ListView(
-          scrollDirection: Axis.vertical,
-          reverse: false,
-          padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-          children: [
-            Text('first'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('sdfdsfd'),
-            Text('last'),
-          ],
-        ),*/
-        /* child: ListView.builder(
-          itemCount: 100, // 100-1 = 99 => 0 -> 99
-          padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-          itemBuilder: (context, index) {
-            return Text('item $index');
-          },
-        ),*/
-        child: ListView.builder(
-          itemCount: friendList.length, // 5 (0-4)
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(friendList[index]),
-            );
-          },
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Floating Action Button');
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
 }
-
-// Suggestion => ctl + space
